e96180a2fee37879e18acd5e63e9b9d9fe4985fd
Michal Kovac <michal.kovac@ncr.com>
2018-10-09 20:04:03 +0200

    MSTest V2 example

---
+++ Examples/ByFramework/MSTest/MSTest.FSharp/FeatureFixture.fs
@@ -0,0 +1,32 @@
+﻿module NUnit.TickSpec
+
+open TickSpec
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+open System.Reflection
+open System.Runtime.ExceptionServices
+
+/// Class containing all BDD tests in current assembly as NUnit unit tests
+[<TestClass>]
+type FeatureFixture () =
+    /// Test method for all BDD tests in current assembly as NUnit unit tests
+    [<TestMethod>]
+    [<DynamicData("Scenarios")>]
+    member __.Bdd (scenario:Scenario) =
+        if scenario.Tags |> Seq.exists ((=) "ignore") then
+            Assert.Inconclusive("Ignored: " + scenario.ToString())
+        try
+            scenario.Action.Invoke()
+        with
+        | :? TargetInvocationException as ex -> ExceptionDispatchInfo.Capture(ex.InnerException).Throw()
+
+    /// All test scenarios from feature files in current assembly
+    static member Scenarios =
+        let assembly = Assembly.GetExecutingAssembly()
+        let definitions = new StepDefinitions(assembly.GetTypes())
+
+        assembly.GetManifestResourceNames()
+        |> Seq.filter (fun (n:string) -> n.EndsWith(".feature") )
+        |> Seq.collect (fun n ->
+            definitions.GenerateFeature(n, assembly.GetManifestResourceStream(n)).Scenarios )
+        |> Seq.map (fun i -> [| i |] )
